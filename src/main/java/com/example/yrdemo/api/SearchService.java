98f529fe2abd683cd34a1d0094c7e05c7fb27cd6
Chapin Nicolas <nchapin@sqli.com>
2018-04-19 13:48:42 +0200

    FIX url encoding

--- src/main/java/com/example/yrdemo/api/SearchService.java
+++ src/main/java/com/example/yrdemo/api/SearchService.java
@@ -7,23 +7,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 @Service
 public class SearchService extends APIService {
     public SearchResultData searchByCategoryCode(SearchQuery query) {
-        URIBuilder builder = new URIBuilder();
-        builder.setPath("/YR-FR/categories/" + query.getCategoryCode() + "/");
-        if (!StringUtils.isEmpty(query.getQuery())) {
-            builder.addParameter("q", query.getQuery());
+        try {
+            URIBuilder builder = new URIBuilder();
+            builder.setPath("/YR-FR/categories/" + query.getCategoryCode() + "/");
+            if (!StringUtils.isEmpty(query.getQuery())) {
+                builder.addParameter("q", query.getQuery());
+            }
+            if (query.getPage() != null) {
+                builder.addParameter("currentPage", query.getPage().toString());
+            }
+            if (!StringUtils.isEmpty(query.getSort())) {
+                builder.addParameter("sort", query.getSort());
+            }
+            String url = null;
+            url = URLDecoder.decode(getAPIURL(builder.toString()), "UTF-8");
+            ResponseEntity<YRAPISearchResponse> response = callRest(url, YRAPISearchResponse.class);
+            return response.getBody().getData();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
-        if (query.getPage() != null) {
-            builder.addParameter("currentPage", query.getPage().toString());
-        }
-        if (!StringUtils.isEmpty(query.getSort())) {
-            builder.addParameter("sort", query.getSort());
-        }
-        String url = getAPIURL(builder.toString()).replaceAll("%3A", ":");
-        ResponseEntity<YRAPISearchResponse> response = callRest(url, YRAPISearchResponse.class);
-        return response.getBody().getData();
+        return null;
     }
 
     public SearchResultData textSearch(String text) {
